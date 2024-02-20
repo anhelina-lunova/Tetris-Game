75025fa79880147f0241dc8a6d073cd2a900c8eb
Anhelina Lunova <120516170+anhelina-lunova@users.noreply.github.com>
2024-02-20 13:24:10 +0200

    add homework

--- script.js
+++ script.js
@@ -79,12 +79,20 @@ function generateTetromino(){
 
     const name = TETROMINO_NAMES[random];
     const matrix = TETROMINOES[name];
+
+    const row = name === 'I' ? -1 : 0;
+    const column =
+    ['T', 'J', 'S'].includes(name)
+    ? playfield[0].length / 2 - Math.floor(matrix[0].length / 2)
+    : playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+
     // console.log(matrix);
     tetromino = {
         name,
         matrix,
-        row: 3,
-        column: 4
+        row,
+        column,
+
     }
 }
 
